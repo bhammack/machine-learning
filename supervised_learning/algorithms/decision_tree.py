2b6bbd767b8e66de1bcb3fa78ab9909073a09e37
Ben Hammack <hammack.b@gmail.com>
2020-01-13 10:48:39 -0500

    Adding adult data set from UCI

---
+++ supervised_learning/algorithms/decision_tree.py
@@ -0,0 +1,19 @@
+import numpy as np
+import sklearn.model_selection as ms
+from sklearn import tree
+
+class DecisionTreeClassifier():
+    """test"""
+    def __init__(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
