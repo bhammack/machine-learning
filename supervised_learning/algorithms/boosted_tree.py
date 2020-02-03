a320567ef4afd429c0b530ec21e40cc9a2bd2217
Ben Hammack <hammack.b@gmail.com>
2020-02-03 16:52:05 -0500

    using max depth of 2

--- supervised_learning/algorithms/boosted_tree.py
+++ supervised_learning/algorithms/boosted_tree.py
@@ -1,11 +1,13 @@
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 # Adaboost requires a tree, so I need to feed it the decisiontree from the other learner
 from . import AbstractLearner
+import numpy as np
 
 class BoostedTreeLearner(AbstractLearner):
     """test"""
     def __init__(self):
-        self.bdt_classifier = AdaBoostClassifier()
+        self.bdt_classifier = AdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=2), n_estimators=600, learning_rate=1.5)
         # boosted trees are iterative
 
     def classifier(self):
@@ -14,4 +16,5 @@ class BoostedTreeLearner(AbstractLearner):
     def experiment(self, xtrain, xtest, ytrain, ytest):
         # plot accuracy vs weak learner count
         pass
-        
+        self.plot_validation_curve(xtrain, ytrain, 'n_estimators', np.arange(2, 151))
+        # self.plot_validation_curve(xtrain, ytrain, 'learning_rate', np.arange(1.0, 11))
