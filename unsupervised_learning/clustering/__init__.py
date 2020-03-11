8ee48746ca1a00aebc93c91c56f9c49468ac6890
Ben Hammack <hammack.b@gmail.com>
2020-03-11 15:54:44 -0400

    working on clustering visualizations

--- unsupervised_learning/clustering/__init__.py
+++ unsupervised_learning/clustering/__init__.py
@@ -2,4 +2,97 @@
 # sklearn calls expectation maximization "gaussian mixture models"
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import KMeans
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+
+from abc import ABC, abstractmethod
+
+# Wow that's nice...
+# https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_digits.html
+
+# https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html#sklearn.cluster.KMeans
+# https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html?highlight=gaussianmixture#sklearn.mixture.GaussianMixture
+
+class AbstractClustering():
+
+    def __init__(self):
+        self.k = None
+
+    @abstractmethod
+    def init(self, k):
+        """Inits the instance of the clustering algorithm."""
+        pass
+
+    @abstractmethod
+    def clustering(self):
+        """Returns the instance of the clustering algorithm."""
+        pass
+
+    def get_params(self):
+        return self.clustering().get_params()
+
+    def set_params(self, **params):
+        return self.clustering().set_params(**params)
+
+    def probability(self, x):
+        return self.clustering().predict_proba(x)
+
+    def experiment(self, xtrain, xtest, ytrain, ytest, X, Y):
+        """Use a custom experiment defined by the learner."""
+        print('No experiment defined!')
+
+    def train(self, x, y):
+        k = len(np.unique(y))
+        print(np.unique(y))
+        print('> discovered {} unique labels in the data'.format(k))
+        self.init(len(np.unique(y)))
+        start = time.time()
+        result = self.clustering().fit(x, y)
+        end = time.time()
+        print('\tfitting data took: {:.5f} secs'.format(end - start))
+        return result
+
+    def test(self, x):
+        return self.clustering().predict(x)
+
+
+class KMeansClustering(AbstractClustering):
+    def init(self, k):
+        self.k = k
+        self.kmeans = KMeans(n_clusters=k)
+
+    def clustering(self):
+        return self.kmeans
+
+    def experiment(self, xtrain, xtest, ytrain, ytest, X, Y):
+        self.train(X, Y)
+        unique = np.unique(Y)
+        labels = self.clustering().predict(X)
+        # plt.scatter(x[:, 0], x[:, 1], c=labels, cmap='viridis')
+        # plt.show()
+        pca = PCA(n_components=self.k).fit_transform(X)
+        serieses = []
+        for i in range(0, pca.shape[0]):
+            print(i)
+            series = plt.text(pca[i,0], pca[i,1], str(unique[i]))
+            serieses.append(series)
+        plt.legend(serieses, unique)
+        # plt.scatter(pca[:,0], pca[:,1], c=labels)
+        plt.show()
+
+
+class EMClustering(AbstractClustering):
+
+    def init(self, k):
+        self.k = k
+        self.em = GaussianMixture(n_components=k)
+
+    def clustering(self):
+        return self.em
+
+    def experiment(self, xtrain, xtest, ytrain, ytest):
+        pass
 
--- unsupervised_learning/main.py
+++ unsupervised_learning/main.py
@@ -1,27 +1,91 @@
 
 import clustering
 import reduction
+import sys
 from data import adults, digits
+import argparse
+import numpy as np
+
+def use_data_set():
+    if args.adults:
+        print('> analyzing the adults data set...')
+        return adults.x_train, adults.x_test, adults.y_train, adults.y_test, adults.x, adults.y
+    elif args.digits:
+        print('> analyzing the digits data set...')
+        return digits.x_train, digits.x_test, digits.y_train, digits.y_test, digits.x, digits.y
 
 
-
-def main():
-    print('test')
-
-
-
-
-
-
-
+def use_clustering_algo(y):
+    if args.kmeans:
+        print('> using k-means clustering...')
+        c = clustering.KMeansClustering()
+    if args.em:
+        print('> using expectation maximization clustering...')
+        c = clustering.EMClustering()
+    return c
 
 
+def cluster():
+    print('Clustering the data...')
+    xtrain, xtest, ytrain, ytest, X, Y = use_data_set()
+    clustering = use_clustering_algo(Y)
+    clustering.experiment(xtrain, xtest, ytrain, ytest, X, Y)
 
 
+def dim_reduce():
+    print('Reducing the dimensions of the data...')
+
+
+def cluster_and_reduce():
+    print('Clustering and reducing the data...')
+
+
+def cluster_and_nn():
+    print('Clustering data and running the neural network...')
+
+
+def reduce_and_nn():
+    print('Applying dimension reduction and running the neural network...')
 
 
 
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Select an experiment to run')
+    parser.add_argument('--kmeans', action='store_true', help='Use the k-means clustering algorithm')
+    parser.add_argument('--em', action='store_true', help='Use the expectation maximization algorithm')
+
+    parser.add_argument('--pca', action='store_true', help='Reduce dimensions using PCA')
+    parser.add_argument('--ica', action='store_true', help='Reduce dimensions using ICA')
+    parser.add_argument('--proj', action='store_true', help='Reduce dimensions using randomized projections')
+    parser.add_argument('--svd', action='store_true', help='Reduce dimensions using single-value decomposition')
+
+    parser.add_argument('--nn', action='store_true', help='Run the neural network on the resultant data')
+
+    parser.add_argument('--adults', action='store_true', help='Experiment with the U.S. adults data set')
+    parser.add_argument('--digits', action='store_true', help='Experiment with the handwritten digits data set')
+
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+    do_clustering = args.kmeans or args.em
+    do_reduction = args.pca or args.ica or args.proj or args.svd
+    do_nn = args.nn
+
+    if do_clustering and not do_reduction:
+        # Only do clustering
+        cluster()
+    if do_reduction and not do_clustering:
+        # Only do reduction
+        dim_reduce()
+    if do_clustering and do_reduction:
+        # Do both clustering and reduction
+        cluster_and_reduce()
+    if do_reduction and do_nn and not do_clustering:
+        # Run a nn on the reduced dimensions
+        reduce_and_nn()
+    if do_clustering and do_nn and not do_reduction:
+        # Run a nn on the clusted data as if it were dim reduction
+        cluster_and_nn()
